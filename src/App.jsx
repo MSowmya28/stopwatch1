ac349315148640810b26ae46e2f18e7e55e04ea2
MSowmya28 <sowmya281203@gmail.com>
2024-07-19 15:33:52 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+import React from 'react';
+import Stopwatch from './Components/stopwatch';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="app">
+      <Stopwatch />
+    </div>
+  );
+};
+
+export default App;
